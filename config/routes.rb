1f906db242e6db98390f255ba6d4ddf00b5a2bc8
Waqar Muhammad Khan <waqarkhan703@gmail.com>
2016-08-09 22:45:23 +0000

    Added pages controller, homeview, and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'pages#home'
+  root 'pages#homePage'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
