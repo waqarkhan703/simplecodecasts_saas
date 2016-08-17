c7653257faab1f3759d0b9180d1c4c8fe69cea55
Waqar Muhammad Khan <waqarkhan703@gmail.com>
2016-08-17 22:00:35 +0000

    Added Validation and create action

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,6 +5,20 @@ class ContactsController < ApplicationController
     end
     
     def create
+        @contact = Contact.new (contact_params)
+        if @contact.save
+            flash[:success] = "Message Sent and saved in Database."
+            redirect_to new_contact_path
+        else
+            flash[:danger] = "Message not sent : Error Occured."
+            redirect_to new_contact_path
+        end
     end
+        
+    
+    private
+        def contact_params
+            params.require(:contact).permit(:name, :email, :comments)
+        end
 
 end
