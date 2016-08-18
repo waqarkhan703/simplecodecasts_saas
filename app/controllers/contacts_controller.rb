356ca893af212ad9dda7c3e357cd50dc85030aee
Waqar Muhammad Khan <waqarkhan703@gmail.com>
2016-08-18 09:13:10 +0000

    Contacts Controller rewritten

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,6 +7,13 @@ class ContactsController < ApplicationController
     def create
         @contact = Contact.new (contact_params)
         if @contact.save
+            
+            name = params[:contact][:name]
+            email = params[:contact][:email]
+            body = params[:contact][:comments]
+            
+            ContactMailer.contact_email(name, email , body).deliver
+            
             flash[:success] = "Message Sent and saved in Database."
             redirect_to new_contact_path
         else
